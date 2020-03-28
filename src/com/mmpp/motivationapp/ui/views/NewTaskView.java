3a9d55ede965596f07d3ec97e3b808066f92a665
Radu Schirliu <radu.schirliu@gmail.com>
2020-03-28 13:47:42 -0600

    Changed button sizes

--- src/com/mmpp/motivationapp/ui/views/NewTaskView.java
+++ src/com/mmpp/motivationapp/ui/views/NewTaskView.java
@@ -4,36 +4,81 @@ import com.mmpp.motivationapp.ui.SceneManager;
 import com.mmpp.motivationapp.ui.SceneView;
 
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class NewTaskView extends SceneView {
+	private String name;
+	private int priority;
+	
 	public NewTaskView(SceneManager sceneManager) {
 		super(sceneManager);
 	}
 
 	@Override
 	public Scene getScene() {
-		StackPane root = new StackPane();
+		BorderPane root = new BorderPane();
 		
-		Button createBtn = new Button("Create");
-		createBtn.setOnAction((ActionEvent event) -> {
-			createTask();
-		});
-		root.getChildren().add(createBtn);
+		// Center pane
+		VBox centerPane = new VBox();
+		
+		Label nameLbl = new Label("Name:");
+		centerPane.getChildren().add(nameLbl);
+		
+		TextField nameField = new TextField();
+		centerPane.getChildren().add(nameField);
+		
+		Label priorityLabel = new Label("Priority:");
+		priorityLabel.setPadding(new Insets(20, 0, 0, 0));
+		centerPane.getChildren().add(priorityLabel);
+		
+		Slider prioritySlider = new Slider(1, 5, 1);
+		prioritySlider.setMinorTickCount(0);
+		prioritySlider.setMajorTickUnit(1);
+		prioritySlider.setSnapToTicks(true);
+		prioritySlider.setShowTickLabels(true);
+		prioritySlider.setShowTickMarks(true);
+		centerPane.getChildren().add(prioritySlider);
+		
+		root.setCenter(centerPane);
+		BorderPane.setMargin(centerPane, new Insets(20));
+		
+		// Bottom pane
+		HBox bottomPane = new HBox();
+		bottomPane.setSpacing(20);
 		
 		Button backBtn = new Button("Back");
+		backBtn.setPrefSize(640, 40);
 		backBtn.setOnAction((ActionEvent event) -> {
 			sceneManager.changeScene("Main");
 		});
-		root.getChildren().add(backBtn);
+		bottomPane.getChildren().add(backBtn);
+		
+		Button createBtn = new Button("Create");
+		createBtn.setPrefSize(640, 40);
+		createBtn.setOnAction((ActionEvent event) -> {
+			name = nameField.getText();
+			priority = (int)prioritySlider.getValue();
+			createTask();
+		});
+		bottomPane.getChildren().add(createBtn);
+		
+		root.setBottom(bottomPane);
+		BorderPane.setMargin(bottomPane, new Insets(0, 20, 20, 20));
 		
 		return new Scene(root, 640, 480);
 	}
 	
 	// TODO: make it do things
 	private void createTask() {
-		
+		System.out.println(name + " " + priority);
+		sceneManager.changeScene("Main");
 	}
 }
