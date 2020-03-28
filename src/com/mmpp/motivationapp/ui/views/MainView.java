39488dcac57dadb473691331b1e3fc2a270a3e59
Radu Schirliu <radu.schirliu@gmail.com>
2020-03-28 12:40:47 -0600

    Added scroll pane, split window into 3

--- src/com/mmpp/motivationapp/ui/views/MainView.java
+++ src/com/mmpp/motivationapp/ui/views/MainView.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -20,29 +21,46 @@ public class MainView extends SceneView {
 	@Override
 	public Scene getScene() {
 		BorderPane root = new BorderPane();
-		root.setPadding(new Insets(20));
+		
+		// Center pane
+		VBox centerPane = new VBox();
+		ScrollPane scrollPane = new ScrollPane();
+		
+		centerPane.getChildren().add(new Text("Tasks:"));
+		centerPane.getChildren().add(scrollPane);
 		
 		VBox taskList = new VBox();
-		taskList.getChildren().add(new Text("Tasks:"));
-		for (int i = 0; i < 10; i++) {
+		
+		for (int i = 0; i < 50; i++) {
 			Button btn = new Button("Test task");
 			btn.setPrefWidth(300);
 			
 			taskList.getChildren().add(btn);
 		}
-		root.setCenter(taskList);
 		
-		StackPane buttonPane = new StackPane();
+		scrollPane.setContent(taskList);
+		root.setCenter(centerPane);
+		BorderPane.setMargin(centerPane, new Insets(20));
+		
+		// Right pane
+		VBox rightPane = new VBox();
+		rightPane.getChildren().add(new Button("something else"));
+		root.setRight(rightPane);
+		BorderPane.setMargin(rightPane, new Insets(20, 20, 0, 0));
+		
+		// Bottom pane
+		StackPane bottomPane = new StackPane();
 		
 		Button newTaskBtn = new Button("New Task");
-		newTaskBtn.setPrefWidth(600);
+		newTaskBtn.setPrefWidth(640);
 		newTaskBtn.setMinHeight(40);
 		newTaskBtn.setOnAction((ActionEvent event) -> {
 			sceneManager.changeScene("NewTask");
 		});
-		buttonPane.getChildren().add(newTaskBtn);
+		bottomPane.getChildren().add(newTaskBtn);
 		
-		root.setBottom(buttonPane);
+		root.setBottom(bottomPane);
+		BorderPane.setMargin(bottomPane, new Insets(0, 20, 20, 20));
 		
 		return new Scene(root, 640, 480);
 	}
