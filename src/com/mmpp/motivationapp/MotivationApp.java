0bc5307b225fb428fd1bba63c6d1faa281b901d1
Tyler Yip <tyler.yip1@ucalgary.ca>
2020-03-29 20:48:43 -0600

    fixed the awkward saving issues lol

--- src/com/mmpp/motivationapp/MotivationApp.java
+++ src/com/mmpp/motivationapp/MotivationApp.java
@@ -30,7 +30,7 @@ public class MotivationApp extends Application {
 	public void start(Stage stage) throws Exception {
 		motivationController = new MotivationController();
 		taskManager = new TaskListManager(motivationController);
-		//achieverController = new AchieverController();
+		achieverController = new AchieverController();
 		
 		sceneManager = new SceneManager(stage);
 		sceneManager.registerScene(new MainView(sceneManager, taskManager, motivationController, achieverController));
@@ -49,6 +49,6 @@ public class MotivationApp extends Application {
 	@Override
 	public void stop() {
 		taskManager.saveListsToFile();
-		//achieverController.saveAchievementsToFile();
+		achieverController.saveAchievementsToFile();
 	}
 }
